7138717d688234904aee08b24b5fa0d93d32279e
Nakrothk <math.palkowski@gmail.com>
2024-03-22 20:09:06 -0300

    main

--- Exercicio13.java
+++ Exercicio13.java
@@ -1,3 +1,38 @@
+import java.util.Scanner;
+
 public class Exercicio13 {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o número de termos da sequência de Fibonacci que deseja gerar: ");
+        int n = scanner.nextInt();
+
+        int[] fibonacci = gerarFibonacci(n);
+
+        System.out.println("Os primeiros " + n + " termos da sequência de Fibonacci são:");
+        for (int termo : fibonacci) {
+            System.out.print(termo + " ");
+        }
+
+        scanner.close();
+    }
+
+    public static int[] gerarFibonacci(int n) {
+        int[] fibonacci = new int[n];
+
+        if (n >= 1) {
+            fibonacci[0] = 1;
+        }
+        if (n >= 2) {
+            fibonacci[1] = 1;
+        }
+
+        for (int i = 2; i < n; i++) {
+            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+        }
+
+        return fibonacci;
+    }
 }
+
